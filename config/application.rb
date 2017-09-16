ac02412c8199709e3e58d8894415fb8c7a6f907a
toyokappa <kppg42@gmail.com>
2017-09-16 22:42:09 +0900

    rubocop -a --only Layout/TrailingWhitespace

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Tabimemo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
