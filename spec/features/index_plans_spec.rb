1d70baa038c732d2fda260c09d8097f7a43586d1
toyokappa <kppg42@gmail.com>
2017-09-18 11:47:26 +0900

    Complete index_plans_spec

--- spec/features/index_plans_spec.rb
+++ spec/features/index_plans_spec.rb
@@ -2,12 +2,27 @@ require "rails_helper"
 
 feature "プラン一覧" do
   context "登録件数が10件の場合" do
+    before do
+      @plans = create_list(:plan, 10)
+      visit root_path
+    end
+
     scenario "1ページに全レコードが表示される" do
+      @plans.each { |plan| expect(page).to have_link href: "/plans/#{plan.id}" }
+      expect(page).not_to have_link href: "/?page=2"
     end
   end
 
   context "登録件数が11件の場合" do
-    scenario "2ページにレコードが表示される" do
+    before do
+      @plan = create_list(:plan, 11)
+      visit root_path
+    end
+
+    scenario "2ページに分割してレコードが表示される" do
+      expect(page).to have_link href: "/plans/#{@plan.first.id}"
+      expect(page).not_to have_link href: "/plans/#{@plan.last.id}"
+      expect(page).to have_link href: "/?page=2"
     end
   end
 end
