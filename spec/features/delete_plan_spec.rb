e55d089a5bd27bae7aaaacb7c7e8c1b8a7bb3603
toyokappa <kppg42@gmail.com>
2017-09-18 12:36:05 +0900

    Complete delete_plan_spec

--- spec/features/delete_plan_spec.rb
+++ spec/features/delete_plan_spec.rb
@@ -1,6 +1,15 @@
 require "rails_helper"
 
 feature "プラン削除" do
+  before { @plan = create(:plan) }
+
   scenario "削除に成功する" do
+    visit root_path
+    expect(page).to have_link href: "/plans/#{@plan.id}"
+    visit plan_path(@plan)
+    click_link I18n.t("link.delete")
+    expect(current_path).to eq root_path
+    expect(page).to have_content I18n.t("flash.delete_success")
+    expect(page).not_to have_link href: "/plans/#{@plan.id}"
   end
 end
