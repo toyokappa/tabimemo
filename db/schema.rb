a270d4d0d52c04b758f0113fabc2a6dc0ba90136
toyokappa <kppg42@gmail.com>
2017-09-20 15:53:06 +0900

    Add photos to spots

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916142001) do
+ActiveRecord::Schema.define(version: 20170920065112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170916142001) do
     t.bigint "plan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "photos"
     t.index ["plan_id"], name: "index_spots_on_plan_id"
   end
 
