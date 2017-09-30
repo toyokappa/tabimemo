a283bc249a6e3deabdb93cd5acda59f99b4412d8
toyokappa <kppg42@gmail.com>
2017-09-30 14:05:41 +0900

    rubocop -a --only Layout/SpaceInsideHashLiteralBraces

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -29,7 +29,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
       end
 
       unless model.instance_variable_get(media_original_filenames_var).map{|k,v| k }.include? original_filename.to_sym
-        new_value = model.instance_variable_get(media_original_filenames_var).merge({"#{original_filename}": SecureRandom.uuid})
+        new_value = model.instance_variable_get(media_original_filenames_var).merge({ "#{original_filename}": SecureRandom.uuid })
         model.instance_variable_set(media_original_filenames_var, new_value)
       end
 
