dc08d25893cc09fc420e505ecc75bf34717d204f
toyokappa <kppg42@gmail.com>
2017-09-30 14:06:51 +0900

    rubocop -a --only Style/Lambda

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,6 @@
 class Plan < ApplicationRecord
   has_many :spots, inverse_of: :plan, dependent: :destroy
-  accepts_nested_attributes_for :spots, reject_if: lambda { |attributes| attributes["name"].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :spots, reject_if: ->(attributes) { attributes["name"].blank? }, allow_destroy: true
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 1000 }
