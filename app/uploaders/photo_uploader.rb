127c0f8c8239b42698809005f42f5ccad40f0d4c
toyokappa <kppg42@gmail.com>
2017-09-30 14:11:03 +0900

    rubocop -a --only Lint/UnusedBlockArgument

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -28,7 +28,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
         model.instance_variable_set(media_original_filenames_var, {})
       end
 
-      unless model.instance_variable_get(media_original_filenames_var).map { |k, v| k }.include? original_filename.to_sym
+      unless model.instance_variable_get(media_original_filenames_var).map { |k, _v| k }.include? original_filename.to_sym
         new_value = model.instance_variable_get(media_original_filenames_var).merge({ "#{original_filename}": SecureRandom.uuid })
         model.instance_variable_set(media_original_filenames_var, new_value)
       end
