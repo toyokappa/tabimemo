8b9c7a568a888fe9a904d409aa49ce8ce53ab463
toyokappa <kppg42@gmail.com>
2017-09-30 12:23:28 +0900

    Setup uploader form

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,5 +1,6 @@
 class Spot < ApplicationRecord
   belongs_to :plan
+  mount_uploaders :photos, PhotoUploader
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 1000 }
