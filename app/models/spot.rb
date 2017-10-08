e67710c917f9c400912793a2d01a3f4b7717c44d
toyokappa <kppg42@gmail.com>
2017-10-09 00:01:56 +0900

    Add photos_file_size validation

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -4,4 +4,15 @@ class Spot < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 1000 }
+  validate :photos_file_size
+
+  private
+
+    def photos_file_size
+      photos.each do |photo|
+        if photo.size > 5.megabytes
+          errors.add(:photos, "のファイルサイズは5MBまでです。")
+        end
+      end
+    end
 end
