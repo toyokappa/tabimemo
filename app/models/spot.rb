798c5d5f652bdcf3905db2acad2c5d1b88fbd13d
Yuta Toyokawa <kppg42@gmail.com>
2017-12-13 19:05:49 +0900

    Change spots table setting

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,9 +2,11 @@ class Spot < ApplicationRecord
   belongs_to :plan
   mount_uploaders :photos, PhotoUploader
 
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, length: { maximum: 50 }
   validates :description, length: { maximum: 1000 }
   validate :photos_file_size
+
+  before_save :set_default_name_with_blank
 
   private
 
@@ -14,5 +16,9 @@ class Spot < ApplicationRecord
           errors.add(:photos, I18n.t("errors.messages.min_size_error"))
         end
       end
+    end
+
+    def set_default_name_with_blank
+      self.name = I18n.t("form.no_spot_name") if name.blank?
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211095601) do
+ActiveRecord::Schema.define(version: 20171213100013) do
 
   create_table "plans", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20171211095601) do
   end
 
   create_table "spots", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name", null: false
+    t.string "name"
     t.text "description"
     t.string "address"
     t.float "latitude", limit: 24
