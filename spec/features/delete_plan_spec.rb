ed1091c972dd7641524d6af7f84d4501ea245b69
toyokappa <kppg42@gmail.com>
2017-12-26 22:50:08 +0900

    Refactor delete_plan_spec

--- spec/features/delete_plan_spec.rb
+++ spec/features/delete_plan_spec.rb
@@ -1,15 +1,28 @@
 require "rails_helper"
 
 feature "プラン削除" do
-  before { @plan = create(:plan) }
+  context "プランを作成したユーザーの場合" do
+    given(:plan) { create(:plan, status: 1) }
 
-  scenario "削除に成功する" do
-    visit root_path
-    expect(page).to have_link href: "/plans/#{@plan.id}"
-    visit plan_path(@plan)
-    click_link I18n.t("link.delete")
-    expect(current_path).to eq root_path
-    expect(page).to have_content I18n.t("flash.delete_success")
-    expect(page).not_to have_link href: "/plans/#{@plan.id}"
+    scenario "プランを削除できる" do
+      sign_in plan.user
+      expect(page).to have_link href: "/plans/#{plan.id}"
+      visit plan_path(plan)
+      click_link I18n.t("link.delete")
+      expect(current_path).to eq users_plans_path
+      expect(page).to have_content I18n.t("flash.delete_success")
+      expect(page).not_to have_link href: "/plans/#{plan.id}"
+    end
+  end
+
+  context "プラン作成したユーザーではない場合" do
+    given(:plan) { create(:plan, status: 1) }
+    given(:other) { create(:user) }
+
+    scenario "プランを削除できない" do
+      sign_in other
+      visit plan_path(plan)
+      expect(page).not_to have_content I18n.t("link.delete")
+    end
   end
 end
