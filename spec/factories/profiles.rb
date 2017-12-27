4284e0fdc0132d2762ea7e0ae600b7902d5a123d
toyokappa <kppg42@gmail.com>
2017-12-27 22:34:57 +0900

    Create edit_profile_spec

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -6,6 +6,7 @@ FactoryGirl.define do
     description { Faker::Lorem.paragraph }
     location { Faker::Address.city }
     url { Faker::Internet.url }
+    gender { [0, 1].sample }
     birthday { Faker::Date.birthday(18,65) }
     image { fixture_file_upload("spec/fixtures/images/sample01.jpg", "image/jpg") }
     user
