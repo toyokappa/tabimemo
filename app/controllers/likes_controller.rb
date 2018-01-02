78728ee7957907a47250484af037d0c100fa8bc2
toyokappa <kppg42@gmail.com>
2018-01-02 22:58:47 +0900

    Add ajax like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,19 @@
 class LikesController < ApplicationController
   def create
-    plan = Plan.find(params[:plan_id])
-    Like.create(user_id: current_user.id, plan_id: plan.id)
-    redirect_to plan_path(plan)
+    @plan = Plan.find(params[:plan_id])
+    Like.create(user_id: current_user.id, plan_id: @plan.id)
+    respond_to do |format|
+      format.html { redirect_to @plan }
+      format.js
+    end
   end
 
   def destroy
-    plan = Plan.find(params[:id])
-    Like.find_by(user_id: current_user, plan_id: plan).destroy
-    redirect_to plan_path(plan)
+    @plan = Plan.find(params[:id])
+    Like.find_by(user_id: current_user, plan_id: @plan).destroy
+    respond_to do |format|
+      format.html { redirect_to @plan }
+      format.js
+    end
   end
 end
