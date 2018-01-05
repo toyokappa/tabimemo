aaaf83eca92e6f8d1950e978fb2c00f9480eb544
Yuta Toyokawa <kppg42@gmail.com>
2018-01-05 22:39:56 +0900

    Refactor comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,22 @@
 class CommentsController < ApplicationController
   def create
-    plan = Plan.find(params[:comment][:plan_id])
-    comment = plan.comments.build(comment_params)
+    comment = current_user.comments.build(comment_params)
     if comment.save
-      redirect_to plan, notice: t("flash.comment_success")
+      redirect_to comment.plan, notice: t("flash.comment_success")
     else
-      redirect_to plan, alert: t("flash.comment_failed")
+      redirect_to comment.plan, alert: t("flash.comment_failed")
     end
   end
 
   def destroy
     comment = Comment.find(params[:id])
-    plan = comment.plan
     comment.destroy
-    redirect_to plan, notice: t("flash.delete_success")
+    redirect_to comment.plan, notice: t("flash.delete_success")
   end
 
   private
 
     def comment_params
-      params.require(:comment).permit(:content, :user_id)
+      params.require(:comment).permit(:content, :plan_id)
     end
 end
