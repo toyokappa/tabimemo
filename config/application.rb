8bee2df4b59a51842ca2df4e8d7bf6bac065b49b
Yuta Toyokawa <kppg42@gmail.com>
2018-02-13 23:08:55 +0900

    Fix google maps api for production

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require "net/http"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,6 +21,8 @@ module Tabimemo
 
     config.active_record.default_timezone = :local
     config.time_zone = "Tokyo"
+
+    config.x.google_apis.map = ENV["GOOGLE_MAPS_API"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
