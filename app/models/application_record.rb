8e36959da0378db5c22fb81e06e50ae3a59f27c4
Yuta Toyokawa <kppg42@gmail.com>
2018-02-24 21:38:31 +0900

    Fix popular plan logic

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  scope :between_1_month, -> { self.where(created_at: [1.month.ago.beginning_of_day..Time.zone.now.end_of_day]) }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :plan
 
   validates :content, presence: true, length: { maximum: 1000 }
+
+  scope :except_plan_user, -> { joins(:plan).where.not("comments.user_id = plans.user_id") }
 end
