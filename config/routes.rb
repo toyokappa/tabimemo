d1a3d2bb02816830d2a28aa3f9d1067e1c342ee3
Yuta Toyokawa <kppg42@gmail.com>
2018-02-26 23:25:10 +0900

    Add terms page

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   resources :plans, only: [:index, :show]
   resources :likes, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy]
+  get "/terms", to: "static#terms"
 end
