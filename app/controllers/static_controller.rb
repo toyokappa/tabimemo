8af20164126435ff8090b10f497c3c0336a11d8e
Yuta Toyokawa <kppg42@gmail.com>
2018-02-27 11:05:12 +0900

    Add privacy policy

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,7 @@
 class StaticController < ApplicationController
   def terms
   end
+
+  def privacy_policy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   resources :likes, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy]
   get "/terms", to: "static#terms"
+  get "/privacy_policy", to: "static#privacy_policy"
 end
