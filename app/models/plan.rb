391065c3e631350e479ec6229e22b166ef9863ed
Yuta Toyokawa <kppg42@gmail.com>
2018-03-15 02:13:04 +0900

    Fix pv logic

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -59,6 +59,7 @@ class Plan < ApplicationRecord
       if @@today != "pv#{Date.today.strftime('%Y_%m_%d')}"
         @@yesterday = "pv#{Date.yesterday.strftime('%Y_%m_%d')}"
         @@today = "pv#{Date.today.strftime('%Y_%m_%d')}"
+        sorted_set @@today, global: true
       end
     end
 end
