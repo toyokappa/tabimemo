fd83ece467535d1d2fb0fa59a687f118593e0711
Yuta Toyokawa <kppg42@gmail.com>
2018-03-18 13:04:00 +0900

    Set i18n for comments mailer

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -2,6 +2,6 @@ class CommentMailer < ApplicationMailer
   def email(comment, plan)
     @comment = comment
     @plan = plan
-    mail to: @plan.user.email, bcc: Rails.configuration.x.mail.to, subject: "【Tabimemo】プランへコメントがつきました"
+    mail to: @plan.user.email, bcc: Rails.configuration.x.mail.to, subject: t("comments.mail.subject")
   end
 end
