e8aac823d4df025cf5753d5c8c6d578f4c79fde3
Yuta Toyokawa <kppg42@gmail.com>
2018-03-29 18:07:04 +0900

    Update docker-compose

---
+++ bin/start_dev_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+echo set crontab
+bundle exec whenever --update-crontab
+
+if [ "${MANUAL}" = "" ]; then
+  RAILS_ENV=${RAILS_ENV} bundle exec rails db:create
+  RAILS_ENV=${RAILS_ENV} bundle exec rails db:migrate
+  rm -f tmp/pids/server.pid
+  RAILS_ENV=${RAILS_ENV} bundle exec rails server -b 0.0.0.0
+else
+  echo "******* MANUAL MODE *******"
+  echo start the server with the following command.
+  echo "# start rails server"
+  echo "railse server -b 0.0.0.0"
+  tail -f < /dev/null
+fi
