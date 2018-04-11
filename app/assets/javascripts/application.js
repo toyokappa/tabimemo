737e0a6f9433f83188f7c2718466da121baf4f48
Yuta Toyokawa <kppg42@gmail.com>
2018-04-11 23:51:37 +0900

    Adjust sp design for top page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require jquery-ui
 //= require jquery-ui/widgets/autocomplete
 //= require jquery-ui/widgets/sortable
+//= require jquery.matchHeight-min
 //= require lodash
 //= require baguetteBox.min
 //= require turbolinks
