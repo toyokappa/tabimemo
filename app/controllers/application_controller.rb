88f13477b361590825ba6782e22d035416abbd70
Yuta Toyokawa <kppg42@gmail.com>
2018-04-14 00:59:19 +0900

    Set meta tag for plans

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include MetaSetter
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
---
+++ app/controllers/concerns/meta_setter.rb
@@ -0,0 +1,11 @@
+module MetaSetter
+  extend ActiveSupport::Concern
+
+  def set_common_meta(key, value)
+    # meta, twitter, ogの3箇所を一気に上書きするためのメソッド
+    # :title, :description, :imageのみ使用可能
+    set_meta_tags key => value,
+                  twitter: { key => "#{value}#{' | Tabimemo' if key == :title}" },
+                  og: { key => value }
+  end
+end
