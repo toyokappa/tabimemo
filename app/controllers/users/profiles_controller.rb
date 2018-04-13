c521110c5d2132bc76b46a5951654c59d9957174
Yuta Toyokawa <kppg42@gmail.com>
2018-04-14 01:12:23 +0900

    Set meta tag for profile

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -2,6 +2,7 @@ class Users::ProfilesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :liked]
   before_action :set_user, only: [:show, :liked]
   before_action :set_profile, only: [:edit, :update]
+  before_action :set_meta, only: [:show]
 
   def show
     @profile = @user.profile
@@ -37,10 +38,20 @@ class Users::ProfilesController < ApplicationController
     end
 
     def set_user
-      @user = User.find_by(name: params[:user_name])
+      @user = User.find_by!(name: params[:user_name])
     end
 
     def set_profile
       @profile = current_user.profile
+    end
+
+    def set_meta
+      case action_name
+      when "show" then
+        title = @user.profile.name.present? ? "#{@user.profile.name}さん" : "#{@user.name}さん"
+        set_common_meta(:title, title)
+        set_common_meta(:description, @user.profile.description) if @user.profile.description.present?
+        set_common_meta(:image, @user.profile.image.url) if @user.profile.image.present?
+      end
     end
 end
