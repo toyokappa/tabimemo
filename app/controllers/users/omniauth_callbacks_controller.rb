2ccde9137755893b22927b3a84dde7a2324efad7
Yuta Toyokawa <kppg42@gmail.com>
2018-04-25 20:32:45 +0900

    Refactor message for sns connection

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,7 +15,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       provider = provider.to_s
       @user = User.find_for_oauth(request.env["omniauth.auth"])
 
-      if @user.persisted?
       if User.find_by(email: @user.email).present?
         redirect_to new_user_session_path, alert: t("devise.omniauth_callbacks.already_registered", kind: provider.capitalize)
       elsif @user.persisted?
@@ -34,7 +33,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       auth = request.env["omniauth.auth"]
       social_account = current_user.social_accounts.build(uid: auth.uid, provider: auth.provider)
       if social_account.save
-        redirect_to edit_user_registration_path, notice: t("devise.omniauth_callbacks.success", kind: provider.capitalize)
+        redirect_to edit_user_registration_path, notice: t("devise.omniauth_callbacks.success_connection", kind: provider.capitalize)
       else
         redirect_to edit_user_registration_path, alert: t("devise.omniauth_callbacks.already_used", kind: provider.capitalize)
       end
