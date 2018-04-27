8cb91f67bf320d93c64cf5470fcbd0a73868c0f4
Yuta Toyokawa <kppg42@gmail.com>
2018-04-27 10:11:04 +0900

    Add setting for sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,27 +1,7 @@
-# Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "https://tabimemo.fun"
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+  Plan.published.find_each do |plan|
+    add plan_path(plan), priority: 1.0, lastmod: plan.updated_at, changefreq: "daily"
+  end
 end
