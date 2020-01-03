78f23ee3397b4af5ade1400fbd87546f9b36da48
Yuta Toyokawa <kppg42@gmail.com>
2020-01-04 01:31:39 +0900

    add tfstate setting

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,27 @@
 provider "aws" {
+  required_version = ">= 0.12.0"
   region  = "ap-northeast-1"
   profile = "tabimemo"
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "tabimemo-terraform-state"
+  versioning {
+    enabled = true
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tabimemo-terraform-state"
+    region = "ap-northeast-1"
+    key = "terraform.tfstate"
+    encrypt = true
+    profile = "tabimemo"
+  }
 }
 
 module "app" {
   source = "./modules"
 }
+
