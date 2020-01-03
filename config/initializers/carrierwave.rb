247b7029264a9fe2482a4336a141ad9daec52b2d
Yuta Toyokawa <kppg42@gmail.com>
2020-01-04 03:13:38 +0900

    fix fog setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,14 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: ENV["AWS_REGION"],
+      region: "ap-northeast-1",
       path_style: true
     }
-    config.fog_public = true
+    config.fog_public = false
     config.fog_attributes = { "Cache-Control": "public, max-age=86400" }
     config.remove_previously_stored_files_after_update = false
-    config.fog_directory = ENV["AWS_S3_BUCKET"]
-    config.asset_host = ENV["AWS_S3_URL"]
+    config.fog_directory = "tabimemo-#{Rails.env}-resources"
+    config.asset_host = Rails.configuration.x.asset_host
   end
 end
 
