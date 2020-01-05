48327bb6d2e5ae3689a388613d8350c172c26798
Yuta Toyokawa <kppg42@gmail.com>
2020-01-06 00:30:09 +0900

    fix iam role

--- terraform/modules/iam_role.tf
+++ terraform/modules/iam_role.tf
@@ -8,8 +8,23 @@ resource "aws_iam_role" "ecs_instance" {
   assume_role_policy = file("policies/ec2_assume_role_policy.json")
 }
 
-resource "aws_iam_role_policy" "ecs_instance" {
-  name = "${local.app_name}-${terraform.workspace}-ecs-instance-role-policy"
+resource "aws_iam_role_policy_attachment" "ecs_service" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
   role = aws_iam_role.ecs_instance.id
-  policy = file("policies/ec2_ecs_control_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "ecr" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role = aws_iam_role.ecs_instance.id
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_power_user" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+  role = aws_iam_role.ecs_instance.id
+}
+
+resource "aws_iam_role_policy" "ssm_get_policy" {
+  name = "${local.app_name}-${terraform.workspace}-ssm-get-policy"
+  role = aws_iam_role.ecs_instance.name
+  policy = templatefile("policies/ssm_get_policy.tmpl", { aws_account_id = local.aws_account_id })
 }
