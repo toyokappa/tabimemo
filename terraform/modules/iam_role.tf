8b584e75eca19fc7b49fc8437093005fabe8a820
Yuta Toyokawa <kppg42@gmail.com>
2020-01-06 11:30:55 +0900

    adjust ecs roles

--- terraform/modules/iam_role.tf
+++ terraform/modules/iam_role.tf
@@ -1,3 +1,4 @@
+# EC2インスタンス用ロール
 resource "aws_iam_instance_profile" "ecs_instance" {
   name = "${local.app_name}-${terraform.workspace}-ecs-instance-profile"
   role = aws_iam_role.ecs_instance.name
@@ -13,8 +14,21 @@ resource "aws_iam_role_policy_attachment" "ecs_service" {
   role = aws_iam_role.ecs_instance.id
 }
 
+# ECS Execution用ロール
+resource "aws_iam_role" "ecs_execution" {
+  name = "${local.app_name}-${terraform.workspace}-ecs-execution-role"
+  assume_role_policy = file("policies/ecs_tasks_assume_role_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+  role       = aws_iam_role.ecs_execution.id
+}
+
 resource "aws_iam_role_policy" "ssm_get_policy" {
   name = "${local.app_name}-${terraform.workspace}-ssm-get-policy"
-  role = aws_iam_role.ecs_instance.name
+  role = aws_iam_role.ecs_execution.name
   policy = templatefile("policies/ssm_get_policy.tmpl", { aws_account_id = local.aws_account_id })
 }
+
+# ECS Task用ロール
