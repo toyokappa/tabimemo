57b7a43e0e5a62040d3eceb88ca789853da753e9
Yuta Toyokawa <kppg42@gmail.com>
2020-01-06 15:49:29 +0900

    create log group

---
+++ terraform/modules/cloudwatch.tf
@@ -0,0 +1,9 @@
+resource "aws_cloudwatch_log_group" "rails_log" {
+  name              = "${local.app_name}-${terraform.workspace}-rails-lg"
+  retention_in_days = "365"
+}
+
+resource "aws_cloudwatch_log_group" "sidekiq_log" {
+  name              = "${local.app_name}-${terraform.workspace}-sidekiq-lg"
+  retention_in_days = "365"
+}
