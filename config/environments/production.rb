7aaeec5c1bb7203cc6744219de645979493a13be
Yuta Toyokawa <kppg42@gmail.com>
2020-01-07 19:07:24 +0900

    configure mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: domain, protocol: "https" }
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mailgun.org",
+    domain: domain,
+    port: 587,
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: :plain,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
--- config/initializers/aws.rb
+++
@@ -1,5 +0,0 @@
-ActionMailer::Base.add_delivery_method :ses,
-  AWS::SES::Base,
-  access_key_id: ENV["AWS_SES_ACCESS_KEY_ID"],
-  secret_access_key: ENV["AWS_SES_SECRET_ACCESS_KEY"],
-  server: "email.us-west-2.amazonaws.com"
