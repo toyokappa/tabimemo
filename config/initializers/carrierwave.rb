59c0ed64098e2191225de6df2e8fe4c57d403236
Yuta Toyokawa <kppg42@gmail.com>
2020-01-07 11:35:44 +0900

    configure carrierwave credential

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,13 +2,14 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: "AWS",
+      use_iam_profile: true,
       region: "ap-northeast-1",
       path_style: true
     }
     config.fog_public = false
     config.fog_attributes = { "Cache-Control": "public, max-age=86400" }
     config.remove_previously_stored_files_after_update = false
-    config.fog_directory = "tabimemo-#{Rails.env}-resources"
+    config.fog_directory = "tabimemo-#{Rails.env}-uploads"
     config.asset_host = Rails.configuration.x.asset_host
   end
 end
