f42653cefc8df7b90d7985ce698195d4dbec955a
Yuta Toyokawa <kppg42@gmail.com>
2020-01-07 20:20:03 +0900

    close ec2 ip

--- terraform/modules/ec2.tf
+++ terraform/modules/ec2.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "ecs_instance" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb.id]
   }
 
   ingress {
