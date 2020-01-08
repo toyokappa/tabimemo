e7ff85b478928490af3c18d3e172c778da9017ad
Yuta Toyokawa <kppg42@gmail.com>
2020-01-08 09:29:25 +0900

    fix pv save bug

--- app/jobs/save_page_views_job.rb
+++ app/jobs/save_page_views_job.rb
@@ -5,7 +5,7 @@ class SavePageViewsJob < ApplicationJob
     # 前日のPVをDBに保存
     Plan.pv.value(with_scores: true).each do |plan_id, count|
       pv = PageView.find_or_initialize_by(date: Date.yesterday, plan_id: plan_id.to_i)
-      pv.count = count
+      pv.count += count
       pv.save!
     end
     Plan.pv.clear
