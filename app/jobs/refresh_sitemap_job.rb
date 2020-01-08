0051aa1c9a1e5e93200d676ac658fa2efc14551e
Yuta Toyokawa <kppg42@gmail.com>
2020-01-09 00:09:53 +0900

    fix sitemap adapter

--- app/jobs/refresh_sitemap_job.rb
+++ app/jobs/refresh_sitemap_job.rb
@@ -2,10 +2,6 @@ class RefreshSitemapJob < ApplicationJob
   queue_as :default
 
   def perform
-    if Rails.env.production?
-      %x(bundle exec rails sitemap:refresh)
-    else
-      %x(bundle exec rails sitemap:refresh:no_ping)
-    end
+    %x(bundle exec rails sitemap:refresh) if Rails.env.production?
   end
 end
